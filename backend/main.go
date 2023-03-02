01a1f008fe6b7699cf060da25cbd693903a48629
rizash <sajmerdenr@gmail.com>
2023-03-02 09:33:29 +0600

    unmarshalled dates, locations

--- backend/main.go
+++ backend/main.go
@@ -13,7 +13,20 @@ func main() {
 		error_log: log.New(os.Stderr, "ERROR:\t", log.Ldate|log.Ltime|log.Lshortfile),
 	}
 
-	data, err := os.ReadFile("./backend/artists.txt")
+	app.Unmarshal()
+
+	my_server := &http.Server{
+		Addr:    ":4001",
+		Handler: app.router(),
+	}
+
+	app.info_log.Println("The server is starting at http://localhost:4001/")
+	app.error_log.Fatalln(my_server.ListenAndServe())
+}
+
+func (app *application) Unmarshal() {
+	// ARTISTS
+	data, err := os.ReadFile("./backend/api/artists.txt")
 	if err != nil {
 		app.error_log.Fatalln(err)
 	}
@@ -23,11 +36,25 @@ func main() {
 		app.error_log.Fatalln(err)
 	}
 
-	my_server := &http.Server{
-		Addr:    ":4001",
-		Handler: app.router(),
+	// DATES
+	data, err = os.ReadFile("./backend/api/dates.txt")
+	if err != nil {
+		app.error_log.Fatalln(err)
 	}
 
-	app.info_log.Println("The server is starting at http://localhost:4001/")
-	app.error_log.Fatalln(my_server.ListenAndServe())
+	err = json.Unmarshal(data, &app.dates)
+	if err != nil {
+		app.error_log.Fatalln(err)
+	}
+
+	// LOCATIONS
+	data, err = os.ReadFile("./backend/api/locations.txt")
+	if err != nil {
+		app.error_log.Fatalln(err)
+	}
+
+	err = json.Unmarshal(data, &app.locations)
+	if err != nil {
+		app.error_log.Fatalln(err)
+	}
 }
--- backend/types.go
+++ backend/types.go
@@ -4,6 +4,14 @@ import (
 	"log"
 )
 
+type application struct {
+	info_log  *log.Logger
+	error_log *log.Logger
+	artists   []artist
+	dates     struct_dates
+	locations struct_locations
+}
+
 type artist struct {
 	Id           int
 	Image        string
@@ -16,8 +24,31 @@ type artist struct {
 	Relations    string
 }
 
-type application struct {
-	info_log  *log.Logger
-	error_log *log.Logger
-	artists   []artist
+type struct_dates struct {
+	Index []date
+}
+
+type date struct {
+	Id    int
+	Dates []string
+}
+
+type struct_locations struct {
+	Index []location
+}
+
+type location struct {
+	Id        int
+	Locations []string
+	Dates     string
+}
+
+type struct_relations struct {
+	Index []relation
+}
+
+type relation struct {
+	Id        int
+	Locations []string
+	Dates     string
 }
